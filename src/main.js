3f8f3c1b7ada837b8562c281cdccc6112cec8ab6
yaoska telleria <yaoskatelleria59@gmail.com>
2024-04-15 14:04:39 -0600

    terminada la primera parte del menu movil

--- src/main.js
+++ src/main.js
@@ -1,3 +1,16 @@
 import { API_KEY } from "./key.js";
 
-// console.log('url' + API_KEY);
+const menu = document.querySelector('#icon-menu');
+const contentMenu = document.querySelector('.menu__links');
+const menuClose = document.querySelector('#icon-menuClose');
+
+menu.addEventListener('click', function () {
+    
+    contentMenu.classList.toggle('inactive');
+    // contentMenu.classList.add('inactive');
+})
+
+menuClose.addEventListener('click', function () {
+    contentMenu.classList.add('inactive');
+})
+
