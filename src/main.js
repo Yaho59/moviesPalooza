e1e3ad6de3653765cc30582f873720a27802eeae
yaoska telleria <yaoskatelleria59@gmail.com>
2024-05-18 15:08:47 -0600

    Validando si se encuntran las peliculas

--- src/main.js
+++ src/main.js
@@ -110,12 +110,17 @@ function createGenres(movies, container) {
 
 // Tendencias
 export async function getTrendingMoviesPreview() {
+    try {
+        const response = await fetch(`${API}trending/movie/day?language=en-US`, options);
+        const data = await response.json();
+        const movies = data.results;
 
-    const response = await fetch(`${API}trending/movie/day?language=en-US`, options);
-    const data = await response.json();
-    const movies = data.results;
+        createMovies(movies, module.trendingMovies);
 
-    createMovies(movies, module.trendingMovies);
+    } catch (error) {
+        console.log("Ha ocurrido un error:", error);
+    }
+
 }
 
 // Generos
@@ -132,12 +137,17 @@ export async function getGenresMovies() {
 
 // filtrado por generos
 export async function getMoviesByCategory(id) {
-    const response = await fetch(`${API}discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&with_genres=${id}`, options);
-    const data = await response.json();
-    const movies = data.results;
-    createMovies(movies, module.listCategory);
+    try {
+        const response = await fetch(`${API}discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&with_genres=${id}`, options);
+        const data = await response.json();
+        const movies = data.results;
+        createMovies(movies, module.listCategory);
 
-    window.addEventListener('scroll', () => getPaginatedMoviesCategory(id));
+        window.addEventListener('scroll', () => getPaginatedMoviesCategory(id));
+
+    } catch (error) {
+        console.log("Ha sucedido un error: ", error);
+    }
 }
 
 let page = 1;
@@ -156,92 +166,116 @@ export async function getPaginatedMoviesCategory(id) {
         const data = await response.json();
         const movie = data.results;
         createMovies(movie, module.listCategory, false);
-        console.log('ajajaja');
-
     }
 }
 
 //Series
 
 export async function getSeriesPreview() {
-    const response = await fetch(`${API}tv/top_rated?language=en-US&page=1`, options);
-    const data = await response.json();
-    const series = data.results;
+    try {
+        const response = await fetch(`${API}tv/top_rated?language=en-US&page=1`, options);
+        const data = await response.json();
+        const series = data.results;
 
-    module.listSerie.innerHTML = ""
+        module.listSerie.innerHTML = ""
 
-    series.forEach(serie => {
-        const movieContainer = document.createElement('div');
-        movieContainer.classList.add('card__movies');
-        movieContainer.addEventListener('click', () => {
-            location.hash = '#serie=' + serie.id;
+        series.forEach(serie => {
+            const movieContainer = document.createElement('div');
+            movieContainer.classList.add('card__movies');
+            movieContainer.addEventListener('click', () => {
+                location.hash = '#serie=' + serie.id;
+            });
+
+            const movieImg = document.createElement('img');
+            movieImg.setAttribute('alt', serie.title);
+            movieImg.setAttribute(
+                'data-src',
+                'https://image.tmdb.org/t/p/w300' + serie.backdrop_path,
+            );
+            lazyLoading.observe(movieImg);
+            const titleMovie = document.createElement('p');
+            titleMovie.classList.add('trending__TitleMovie')
+            titleMovie.textContent = serie.name;
+
+            const content_vote = document.createElement('div');
+            content_vote.classList.add('content_vote');
+            content_vote.classList.add('flex')
+
+
+            const iconStar = document.createElement('img');
+            iconStar.src = '../icons/star_icon.svg';
+
+            const vote = document.createElement('span');
+            vote.textContent = `${serie.vote_average.toFixed(1)}/10`;
+            vote.classList.add('spanVote');
+
+            content_vote.append(vote, iconStar)
+
+            movieContainer.append(movieImg, titleMovie, content_vote);
+            module.listSerie.append(movieContainer);
+
         });
-
-        const movieImg = document.createElement('img');
-        movieImg.setAttribute('alt', serie.title);
-        movieImg.setAttribute(
-            'data-src',
-            'https://image.tmdb.org/t/p/w300' + serie.backdrop_path,
-        );
-        lazyLoading.observe(movieImg);
-        const titleMovie = document.createElement('p');
-        titleMovie.classList.add('trending__TitleMovie')
-        titleMovie.textContent = serie.name;
-
-        const content_vote = document.createElement('div');
-        content_vote.classList.add('content_vote');
-        content_vote.classList.add('flex')
-
-
-        const iconStar = document.createElement('img');
-        iconStar.src = '../icons/star_icon.svg';
-
-        const vote = document.createElement('span');
-        vote.textContent = `${serie.vote_average.toFixed(1)}/10`;
-        vote.classList.add('spanVote');
-
-        content_vote.append(vote, iconStar)
-
-        movieContainer.append(movieImg, titleMovie, content_vote);
-        module.listSerie.append(movieContainer);
-
-    });
+    } catch (error) {
+        console.log("Ha ocurrido un error: ", error);
+    }
 }
 
 // Peliculas por busqueda
 export async function getMoviesBySearch(query) {
-    const response = await fetch(`${API}search/movie?query=${query}&include_adult=true&language=en-US&page=1`, options);
-    const data = await response.json();
-    const movies = data.results;
+    const message = document.querySelector('#message');
+    try {
+        const response = await fetch(`${API}search/movie?query=${query}&include_adult=true&language=en-US&page=1`, options);
+        const data = await response.json();
+        const movies = data.results;
 
-    createMovies(movies, module.listCategory);
+        if (response.status === 200) {
+
+            if (movies.length > 0) {
+                createMovies(movies, module.listCategory);
+            } else {
+                message.innerHTML = "Movie not found"
+            }
+        } else {
+            message.innerHTML = "An error occurred while fetching the data.";
+        }
+
+    } catch (error) {
+
+        console.log("Ha ocurrido un error: ", error)
+    }
 }
 
 // Detalles de una pelicula
 export async function getMovieDetail(movieId) {
-    const response = await fetch(`${API}movie/${movieId}?language=en-US`, options);
-    const data = await response.json();
-    const movie = data;
+    try {
+        const response = await fetch(`${API}movie/${movieId}?language=en-US`, options);
+        const data = await response.json();
+        const movie = data;
 
-    module.movieDetailCategoriesList.innerHTML = '';
+        module.movieDetailCategoriesList.innerHTML = '';
 
-    const url = `https://image.tmdb.org/t/p/w1280${movie.backdrop_path}`;
-    module.imgDetail.src = url;
-    module.poster.src = url;
+        const url = `https://image.tmdb.org/t/p/w1280${movie.backdrop_path}`;
+        module.imgDetail.src = url;
+        module.poster.src = url;
 
-    module.detailTitle.textContent = movie.original_title;
-    module.movieDetailDescrip.textContent = movie.overview;
-    module.movieDetailScore.textContent = movie.vote_average.toFixed(1);
+        module.detailTitle.textContent = movie.original_title;
+        module.movieDetailDescrip.textContent = movie.overview;
+        module.movieDetailScore.textContent = movie.vote_average.toFixed(1);
 
-    movie.genres.forEach(genre => {
+        movie.genres.forEach(genre => {
 
-        const nameCategory = document.createElement('span');
-        nameCategory.textContent = genre.name
+            const nameCategory = document.createElement('span');
+            nameCategory.textContent = genre.name
 
-        module.movieDetailCategoriesList.append(nameCategory);
-    });
+            module.movieDetailCategoriesList.append(nameCategory);
+        });
 
-    getRecommendationsMovies(movieId);
+        getRecommendationsMovies(movieId);
+
+    } catch (error) {
+        console.log("Ha ocurrido un error: ", error);
+    }
+
 }
 
 
